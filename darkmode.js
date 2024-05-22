5398c370c8c56a91a1d743c8a25f7fc11aea3f43
Irene Bouslama <irenebouslama@gmail.com>
2024-05-22 11:10:49 +0200

    Implémentation darkmode

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let theme = localStorage.getItem("theme") || false;
+    if (theme) document.documentElement.setAttribute("data-theme", theme);
+});
 
 document.getElementById('darkMode').onclick = () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
     document.getElementById('light-dark').setAttribute('style', 'rotate: 180deg');
 };
+
