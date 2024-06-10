b253f3e2e7b72bb99da59d404c2de704fac5b710
Irene Bouslama <irenebouslama@gmail.com>
2024-06-10 17:40:15 +0200

    Responsive mobile

--- darkmode.js
+++ darkmode.js
@@ -3,11 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (theme) document.documentElement.setAttribute("data-theme", theme);
 });
 
-document.getElementById('darkMode').onclick = () => {
+document.getElementById('darkmode-checkbox').onclick = () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
-    document.getElementById('light-dark').setAttribute('style', 'rotate: 180deg');
 };
-
